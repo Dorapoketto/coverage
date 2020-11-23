bca7bae86e09caa3c1c94a7dc0b1e3cbbf7ece9f
Dorapoketto <mxc2012@outlook.com>
2020-11-23 19:28:11 +0800

    增加生成report的method

--- src/main/java/com/dora/coverage/Coverage.java
+++ src/main/java/com/dora/coverage/Coverage.java
@@ -1,7 +1,13 @@
 package com.dora.coverage;
 
+import java.io.File;
+
 public class Coverage {
     public static void main(String[] args) {
-        
+        ToExec toExec = new ToExec();
+//        toExec.dump("./jacoco.exec", "10.30.30.77", 38333);
+
+
+        ToReport toReport = new ToReport(new File("/Users/vincent/Desktop/isc-app-market-service"));
     }
 }
--- src/main/java/com/dora/coverage/ToReport.java
+++ src/main/java/com/dora/coverage/ToReport.java
@@ -1,6 +1,77 @@
 package com.dora.coverage;
 
+import org.jacoco.core.analysis.Analyzer;
+import org.jacoco.core.analysis.CoverageBuilder;
+import org.jacoco.core.analysis.IBundleCoverage;
+import org.jacoco.core.tools.ExecFileLoader;
+import org.jacoco.report.IReportVisitor;
+import org.jacoco.report.html.HTMLFormatter;
+
+import java.io.File;
+import java.io.IOException;
+
 public class ToReport {
 
+    /**
+     * 覆盖率文件地址
+     */
+    private final File executionDataFile;
 
+    /**
+     * 代码编译后jar包所在位置
+     */
+    private final File classesDirectory;
+
+    /**
+     * 源代码路径
+     */
+    private final File sourceDirectory;
+
+    /**
+     * report 生成路径
+     */
+    private final File reportDirectory;
+    private ExecFileLoader execFileLoader;
+
+
+    /**
+     * Create a new generator based for the given project.
+     *
+     * @param projectDirectory 项目路径
+     */
+    public ToReport(final File projectDirectory) {
+        this.executionDataFile = new File(projectDirectory, "jacoco.exec");
+        this.classesDirectory = new File(projectDirectory, "target");
+        this.sourceDirectory = new File(projectDirectory, "src");
+        this.reportDirectory = new File(projectDirectory, "coveragereport");
+    }
+
+    public static void generateReport() {
+
+        final HTMLFormatter htmlFormatter = new HTMLFormatter();
+        final IReportVisitor iReportVisitor = null;
+
+
+    }
+
+
+    /**
+     * 加载exec文件
+     *
+     * @throws IOException
+     */
+    private void loadExecutionData() throws IOException {
+        execFileLoader = new ExecFileLoader();
+        execFileLoader.load(executionDataFile);
+    }
+
+    private IBundleCoverage analyzeStructure() throws IOException {
+        final CoverageBuilder coverageBuilder = new CoverageBuilder();
+        final Analyzer analyzer = new Analyzer(
+                execFileLoader.getExecutionDataStore(), coverageBuilder);
+
+        analyzer.analyzeAll(classesDirectory);
+
+        return coverageBuilder.getBundle("app-market");
+    }
 }
