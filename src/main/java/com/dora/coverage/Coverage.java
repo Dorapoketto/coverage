00f773b1a50fc68ee912825c6f24d93839364a7a
Dorapoketto <mxc2012@outlook.com>
2020-11-24 14:04:06 +0800

    生成report 的方法基本完成，待完善

--- src/main/java/com/dora/coverage/Coverage.java
+++ src/main/java/com/dora/coverage/Coverage.java
@@ -1,13 +1,20 @@
 package com.dora.coverage;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Coverage {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String file_path = "/Users/vincent/Desktop/jacoco/isc-app-market-service/jacoco.exec";
+
         ToExec toExec = new ToExec();
-//        toExec.dump("./jacoco.exec", "10.30.30.77", 38333);
+        toExec.dump(file_path, "10.30.30.77", 38333);
 
 
-        ToReport toReport = new ToReport(new File("/Users/vincent/Desktop/isc-app-market-service"));
+
+        ToReport toReport = new ToReport(new File("/Users/vincent/Desktop/jacoco/isc-app-market-service"));
+
+        toReport.loadExecutionData();
+        toReport.generateReport();
     }
 }
--- src/main/java/com/dora/coverage/ToReport.java
+++ src/main/java/com/dora/coverage/ToReport.java
@@ -4,6 +4,8 @@ import org.jacoco.core.analysis.Analyzer;
 import org.jacoco.core.analysis.CoverageBuilder;
 import org.jacoco.core.analysis.IBundleCoverage;
 import org.jacoco.core.tools.ExecFileLoader;
+import org.jacoco.report.DirectorySourceFileLocator;
+import org.jacoco.report.FileMultiReportOutput;
 import org.jacoco.report.IReportVisitor;
 import org.jacoco.report.html.HTMLFormatter;
 
@@ -31,6 +33,7 @@ public class ToReport {
      * report 生成路径
      */
     private final File reportDirectory;
+
     private ExecFileLoader execFileLoader;
 
 
@@ -46,25 +49,38 @@ public class ToReport {
         this.reportDirectory = new File(projectDirectory, "coveragereport");
     }
 
-    public static void generateReport() {
+    public void generateReport() throws IOException {
 
         final HTMLFormatter htmlFormatter = new HTMLFormatter();
-        final IReportVisitor iReportVisitor = null;
+        final IReportVisitor iReportVisitor = htmlFormatter.createVisitor(new FileMultiReportOutput(reportDirectory));
 
+        final IBundleCoverage iBundleCoverage = analyzeStructure();
 
+        iReportVisitor.visitInfo(execFileLoader.getSessionInfoStore().getInfos(),
+                execFileLoader.getExecutionDataStore().getContents());
+
+        iReportVisitor.visitBundle(iBundleCoverage,
+                new DirectorySourceFileLocator(sourceDirectory, "utf-8", 4));
+
+        iReportVisitor.visitEnd();
     }
 
 
     /**
      * 加载exec文件
      *
-     * @throws IOException
+     * @throws IOException 文件异常
      */
-    private void loadExecutionData() throws IOException {
+    public void loadExecutionData() throws IOException {
         execFileLoader = new ExecFileLoader();
         execFileLoader.load(executionDataFile);
     }
 
+    /**
+     *
+     * @return coverageBuilder
+     * @throws IOException 文件异常
+     */
     private IBundleCoverage analyzeStructure() throws IOException {
         final CoverageBuilder coverageBuilder = new CoverageBuilder();
         final Analyzer analyzer = new Analyzer(
